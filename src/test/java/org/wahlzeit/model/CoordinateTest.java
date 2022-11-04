117b3c17e42b81127f79f04518055dbffd95ecb4
cat24max <tim.vogler@fau.de>
2022-11-04 11:44:49 +0100

    CoordinateTest extended with new features

--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -60,6 +60,56 @@ public class CoordinateTest {
         assertFalse(new CartesianCoordinate(2, 1, 3).equals(coord));
 	}
 
+	/**
+	 *
+	 */
+	@Test
+	public void testCartesianToSphericConversion() {
+        CartesianCoordinate coord = new CartesianCoordinate(2, 1, 0);
+		SphericCoordinate sph = coord.asSphericCoordinate();
+
+		assertEquals((Double) sph.getPhi(), (Double) 0.4636476, 0.01);
+		assertEquals((Double) sph.getTheta(), (Double) 1.5707963, 0.01);
+		assertEquals((Double) sph.getRadius(), (Double) 2.23606, 0.01);
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testCartesianToSphericToCartesianConversion() {
+        CartesianCoordinate coord = new CartesianCoordinate(5, 13.5, 420.69);
+		SphericCoordinate sph = coord.asSphericCoordinate();
+		CartesianCoordinate backConverted = sph.asCartesianCoordinate();
+
+		assertEquals((Double) coord.getX(), (Double) backConverted.getX(), 0.01);
+		assertEquals((Double) coord.getY(), (Double) backConverted.getY(), 0.01);
+		assertEquals((Double) coord.getZ(), (Double) backConverted.getZ(), 0.01);
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testCentralAngle() {
+		// Using a cartesian coord here on purpose
+		CartesianCoordinate coord1 = new CartesianCoordinate(2, 1, 0);
+        CartesianCoordinate coord2 = new CartesianCoordinate(5, 13.5, 420.69);
+		
+		double angleSame1 = coord1.getCentralAngle(coord1);
+		double angleSame2 = coord2.getCentralAngle(coord2);
+
+		double angle = coord1.getCentralAngle(coord2);
+		double angleFlipped = coord1.getCentralAngle(coord2);
+
+		assertEquals((Double) 0.0, (Double) angleSame1);
+		assertEquals((Double) 0.0, (Double) angleSame2);
+		assertEquals((Double) 1.54582, (Double) angle, 0.001);
+		assertEquals((Double) 1.54582, (Double) angleFlipped, 0.001);
+	}
+
+	
+
 
 
 

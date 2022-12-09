236d7746d14b310873a405bfab455ede3d716a5c
cat24max <tim.vogler@fau.de>
2022-12-09 13:29:35 +0100

    Prepare CW10 classes

---
+++ src/main/java/org/wahlzeit/model/PlaneManager.java
@@ -0,0 +1,17 @@
+package org.wahlzeit.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.wahlzeit.services.ObjectManager;
+import org.wahlzeit.services.Persistent;
+
+public class PlaneManager extends ObjectManager {
+
+    @Override
+    protected Persistent createObject(ResultSet rset) throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
---
+++ src/test/java/org/wahlzeit/model/PlaneTest.java
@@ -0,0 +1,11 @@
+package org.wahlzeit.model;
+
+import org.junit.Test;
+
+/**
+ * Test cases for the Gender class.
+ */
+public class PlaneTest {
+
+
+}

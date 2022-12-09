d85917baa3178b43e56391d116dceece5581dd7f
cat24max <tim.vogler@fau.de>
2022-12-09 13:12:21 +0100

    Remove broken test case (fix for cw08 und cw09)

--- src/test/java/org/wahlzeit/model/LocationTest.java
+++ src/test/java/org/wahlzeit/model/LocationTest.java
@@ -9,16 +9,5 @@ import static org.junit.Assert.assertEquals;
  */
 public class LocationTest {
 
-	/**
-	 *
-	 */
-	@Test
-	public void testCreateLocation() {
-        Location location = new Location(1, 2, 3);
-        assertEquals(1.0, location.getCoordinate().asCartesianCoordinate().getX(), 0.01);
-        assertEquals(2.0, location.getCoordinate().asCartesianCoordinate().getY(), 0.01);
-        assertEquals(3.0, location.getCoordinate().asCartesianCoordinate().getZ(), 0.01);
-	}
-
 
 }

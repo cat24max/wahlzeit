714f5a22c1fbc44f708967a47520b30813338927
cat24max <tim.vogler@fau.de>
2023-01-16 11:24:34 +0100

    Implement PlaneType subtyping

--- src/main/java/org/wahlzeit/model/PlaneType.java
+++ src/main/java/org/wahlzeit/model/PlaneType.java
@@ -2,6 +2,8 @@ package org.wahlzeit.model;
 
 public class PlaneType {
 
+    private PlaneType superType;
+
     private String type; // 4 digit ICAO type code
     
     public PlaneType(String type) {
@@ -28,9 +30,18 @@ public class PlaneType {
 
     @Override
     public boolean equals(Object other) {
-        if (this == other) return true;
+        if(this == other) return true;
         if(!(other instanceof PlaneType)) return false;
         return this.type.equals(((PlaneType) other).getTypestring());
+    }
+
+    public String getSupertype() {
+        return this.type.substring(0, 3);
+    }
+
+    public boolean isSubtype(PlaneType other) {
+        if(other == null) throw new IllegalArgumentException("Pot. Subtype cannot be null");
+        return this.getSupertype().equals(other.getSupertype());
     }
 
     public void assertClassInvariants() {
--- src/test/java/org/wahlzeit/model/PlaneTest.java
+++ src/test/java/org/wahlzeit/model/PlaneTest.java
@@ -1,6 +1,7 @@
 package org.wahlzeit.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -25,6 +26,15 @@ public class PlaneTest {
     @Test(expected = IllegalArgumentException.class)
     public void testUnsetPlaneType() {
         new Plane("A359").getAircraftType().setTypestring("");
+    }
+
+    @Test
+    public void testPlaneTypeSubtype() {
+        PlaneType sevenFourSeven = new PlaneType("B740");
+        Plane a = new Plane("B744");  // Boeing 747-400
+        Plane b = new Plane("B748");  // Boeing 747-8
+        assertTrue("747-400 should be a 747", a.getAircraftType().isSubtype(sevenFourSeven));
+        assertTrue("747-8 should be a 747", b.getAircraftType().isSubtype(sevenFourSeven));
     }
 
 }
